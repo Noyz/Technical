d7d3ec79a12e60447403f152f81eebb5ba2d66c1
noyz <noyz-97160@live.fr>
2019-12-20 14:40:03 +0100

    cart react prod + rearrange all file

---
+++ cart-react.juliannoyon.com/build/precache-manifest.df733d89da0ba1e5d496d3b215445e01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b079f75a0001af40b9ed01a4f29a3dae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "070eb8834ee3a00aad8c",
+    "url": "/static/css/main.e8f6d0dd.chunk.css"
+  },
+  {
+    "revision": "7aade89bf0051008daec",
+    "url": "/static/js/2.c99829ef.chunk.js"
+  },
+  {
+    "revision": "070eb8834ee3a00aad8c",
+    "url": "/static/js/main.311f62aa.chunk.js"
+  },
+  {
+    "revision": "53a3c68e81095b6850f6",
+    "url": "/static/js/runtime-main.07b9fe40.js"
+  }
+]);
---
+++ cart-react.juliannoyon.com/public/precache-manifest.8c468c5ea9d565527b42efa89a5d84e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90add9cbf91ce99c4c62cdcae590d08a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d129a675c2bde7f439c5",
+    "url": "/static/css/main.e8f6d0dd.chunk.css"
+  },
+  {
+    "revision": "e449bb71692e533f624b",
+    "url": "/static/js/2.08d579c6.chunk.js"
+  },
+  {
+    "revision": "d129a675c2bde7f439c5",
+    "url": "/static/js/main.e870a004.chunk.js"
+  },
+  {
+    "revision": "8d43d82ccf601f8df8bb",
+    "url": "/static/js/runtime-main.e0638a80.js"
+  }
+]);
